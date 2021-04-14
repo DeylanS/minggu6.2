453664be93bb37a6dc90dfdf3716e7ba9d601f3f
Deylan Shiva <18DDT19F1066@student.ptss.edu.my>
2021-04-14 12:11:42 +0800

    Array

---
+++ minggu6.2/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstHari = New System.Windows.Forms.ListBox()
+        Me.btnHari = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstHari
+        '
+        Me.lstHari.FormattingEnabled = True
+        Me.lstHari.Location = New System.Drawing.Point(340, 124)
+        Me.lstHari.MultiColumn = True
+        Me.lstHari.Name = "lstHari"
+        Me.lstHari.Size = New System.Drawing.Size(120, 95)
+        Me.lstHari.TabIndex = 3
+        '
+        'btnHari
+        '
+        Me.btnHari.Location = New System.Drawing.Point(368, 262)
+        Me.btnHari.Name = "btnHari"
+        Me.btnHari.Size = New System.Drawing.Size(75, 23)
+        Me.btnHari.TabIndex = 4
+        Me.btnHari.Text = "Papar"
+        Me.btnHari.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnHari)
+        Me.Controls.Add(Me.lstHari)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lstHari As ListBox
+    Friend WithEvents btnHari As Button
+End Class
---
+++ minggu6.2/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub btnHari_Click(sender As Object, e As EventArgs) Handles btnHari.Click
+        Dim strNamaHari(6) As String
+        Dim x As Integer
+        strNamaHari(0) = "Ahad"
+        strNamaHari(1) = "Isnin"
+        strNamaHari(2) = "Selasa"
+        strNamaHari(3) = "Rabu"
+        strNamaHari(4) = "Khamis"
+        strNamaHari(5) = "Jumaat"
+        strNamaHari(6) = "Sabtu"
+        x = 0
+        Array.Sort(strNamaHari)
+
+        For x = 0 To 6
+            lstHari.Items.Add(strNamaHari(x))
+        Next
+    End Sub
+End Class
